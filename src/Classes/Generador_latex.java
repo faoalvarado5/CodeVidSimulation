f21ff051884edd1a06735b24ab821d9022932e38
Emanuelle <emanuellejs@hotmail.es>
2020-06-06 22:54:26 -0600

    Se fraccionó el código entre multi jugador y un solo jugador
    Ya se pueden conectar los servidores con los clientes

---
+++ src/Classes/Generador_latex.java
@@ -0,0 +1,79 @@
+package Classes;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Generador_latex {
+
+    public void generarLatex() throws IOException {
+
+        File archivo = new File("latex/salida.tex");
+        if (!archivo.exists()) {
+            try {
+                archivo.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FileWriter fileWriter = null;
+
+        try {
+            fileWriter = new FileWriter(archivo, false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String contents[] = new File("latex/").list();
+
+        // Este es el string para generar la portada del documento de salida.
+        String documento = "\"" + "documentclass{report}" + "\n" +
+                "\"" + "usepackage{graphicx}" + "\n" +
+                "\"" + "begin{document}" + "\n" +
+                "\"" + "begin{titlepage}" + "\n" +
+                "\"" + "centering" + "\n" +
+                "{\"" + "bfseries\\LARGE Instituto Tecnol\\'ogico de Costa Rica \\par}" + "\n" +
+                "\"" + "vspace{1cm}" + "\n" +
+                "{\"" + "scshape\\Large Facultad de Ingenier\\'ia en Computaci\\'on \\par}" + "\n" +
+                "\"" + "vspace{3cm}" + "\n" +
+                "{\"" + "scshape\\Huge Simulaci\\'on de propagaci\\'on \\\\" + "\n" +
+                "de COVID-19\\par}" + "\n" +
+                "\"" + "vspace{3cm}" + "\n" +
+                "{\"" + "itshape\\Large Proyecto 2\\par}" + "\n" +
+                "\"" + "vfill" + "\n" +
+                "{\"" + "Large Emanuelle Jim\\'enez S.\\par}" + "\n" +
+                "{\"" + "Large Fabrizio Alvarado B\\par}" + "\n" +
+                "\"" + "vfill" + "\n" +
+                "{\"" + "Large Junio 2020 \\par}" + "\n" +
+                "\"" + "end{titlepage}" + "\n" +
+                "\"" + "newpage" + "\n" +
+                "\"" + "section{Gr\\'afico}" + "\n" +
+
+                "\"" + "includegraphics{" + contents[0] + "}" + "\n" +
+
+                "\"" + "newpage" + "\n" +
+                "\"" + "section{Cambios en el mapa}" + "\n";
+
+        for (int i = 1; i < contents.length - 1; i++) {
+
+            documento += "\"" + "includegraphics{" + contents[i] + "}" + "\n";
+        }
+
+        documento += "\"" + "end{document}" + "\n";
+
+        documento = documento.replaceAll("\"", "\\\\");
+        try {
+            fileWriter.write(documento);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JOptionPane.showMessageDialog(null, "Latex generado con exito.");
+    }
+}
---
+++ src/Multimap/ServerGui.java
@@ -0,0 +1,87 @@
+package Multimap;
+
+import Classes.agente;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+public class ServerGui {
+
+    JFrame f;
+
+    JButton start_button = new JButton("Ready to start");//creating instance of JButton
+    JLabel label = new JLabel("Looking for clients...");
+    JLabel label_of_countries_connected = new JLabel("");
+
+    public ServerGui() {
+
+        f = new JFrame("Simulacion de propagacion de COVID-19");//creating instance of JFrame
+
+        start_button.setBounds(150, 200, 150, 40);
+        label.setBounds(150, 100, 150, 40);
+        label_of_countries_connected.setBounds(150, 50, 150, 40);
+
+        start_button.addActionListener(new StartServerGui());
+
+        f.add(label_of_countries_connected);
+        f.add(start_button);
+        f.add(label);
+
+        f.setSize(500, 400);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
+        f.setLocationRelativeTo(null);
+
+    }
+
+
+    class StartServerGui implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+
+                        ServerSocket serverSocket = new ServerSocket(1234);
+
+                        int contador = 0;
+
+                        while (contador < 5) {
+
+                            TimeUnit.SECONDS.sleep(1);
+                            contador++;
+                            label.setText("Looking for clients... " + contador);
+                            Socket socket = serverSocket.accept();
+
+                            ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+                            ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+
+                            String recvPacket = (String) inStream.readObject();
+                            label_of_countries_connected.setText(label_of_countries_connected.getText() + ", " + recvPacket);
+
+                            String msg = "¿Hi, are you ready to try new diseases?";
+                            outStream.writeObject(msg);
+
+                            if(socket.isConnected())
+                                start_button.setEnabled(true);
+
+                        }
+                    }catch (Exception error){
+                        JOptionPane.showMessageDialog(null, "Something went wrong...");
+                    }
+                }
+            });
+
+            thread.start();
+
+        }
+    }
+
+}
